6ce7abf35f41cc18dedb5ce042c67a6f8233ee02
Matt Roberts <matthew.roberts@bestgateeng.com>
2022-09-01 13:15:39 -0400

    updated main

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from twisted.web.server import Site
 
 class FormPage(Resource):
     def render_GET(self, request):
+        # Do some work
         return b"hit"
 
     def render_POST(self, request):
         pprint(request.__dict__)
         newdata = request.content.getvalue()
+        # Do some work
         return newdata
 
 
